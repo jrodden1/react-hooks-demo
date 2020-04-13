4d08f21c748b44055713b03e377a84fafd08e416
Jeremiah <39208179+jrodden1@users.noreply.github.com>
2020-04-13 15:36:50 -0600

    Examples added comparing componentDidMount()/componentWillMount() from a class to useEffect from hooks

--- src/GreetingClassFormInputs2.js
+++ src/GreetingClassFormInputs2.js
@@ -6,9 +6,11 @@ export default class Greeting extends Component {
    constructor(props){
       super(props)
       this.state = {
-         name: 'Mary'
+         name: 'Mary',
+         lastname: 'Poppins'
       }
       this.handleNameChange = this.handleNameChange.bind(this)
+      this.handleLastnameChange = this.handleLastnameChange.bind(this)
    }
 
    handleNameChange(e) {
@@ -16,7 +18,27 @@ export default class Greeting extends Component {
          name: e.target.value
       })
    }
-  
+
+   handleLastnameChange(e) {
+      this.setState({
+         lastname: e.target.value
+      })
+   }
+
+   // timestamp: 34:25 https://www.youtube.com/watch?v=dpw9EHDh2bM
+   // Lifecycle methods example:  componentDidMount, componentWillUnmount, etc
+   /* List of Lifecycle methodscomponentWillMount() {}; componentDidMount() {}; componentWillReceiveProps(nextProps) {}; shouldComponentUpdate(nextProps, nextState) {}; componentWillUpdate(nextProps, nextState) {}; componentDidUpdate(prevProps, prevState) {}; componentWillUnmount() {}; */
+   // Lifecycle methods create side effects based on a condition.  
+   // For example, if/when the component, did mount, then do something (you are actually spelling out what to do inside the componentDidMount() function; essentially, this is a callback function that is run when the component mounts)
+   // Example Side Effects for changing the title of the browser tab
+   componentDidMount(){
+      document.title = this.state.name + " " + this.state.lastname
+   }
+
+   componentDidUpdate(){
+      document.title = this.state.name + " " + this.state.lastname
+   }
+
    render() {
       return (
          <div>
@@ -24,6 +46,10 @@ export default class Greeting extends Component {
             <input
                value={this.state.name}
                onChange={this.handleNameChange}
+            /> 
+            <input
+               value={this.state.lastname}
+               onChange={this.handleLastnameChange}
             /> 
          </div>
       )
--- src/GreetingHookifiedFormInputs3.js
+++ src/GreetingHookifiedFormInputs3.js
@@ -1,17 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 //Timestamp: 20:14
 //youtube.com/watch?v=dpw9EHDh2bM
 
 export default function GreetingHookifiedFormInputs3() {
+   // KEY RULE: Hooks must be declared at the top of your component -- you cannot have any other logic before the declaration of Hooks. 
+   
    // This is declaring the name of the attribute I want to track state on
    // and the name I want to give to the state setter method - setName
    // This will use React's local state -->> this is what "useState" does - use a local state for this component
    // useState is a "hook" -> a Hook is a fn provided by React that lets you "hook" into React features from your functional components
+   // Whenever one of the states change or the setter method is called, then react will rerender the component just like it does with this.setState
    const [ name, setName  ] = useState('Mary'/* Initial State is set here */)
+   const [ lastname, setLastname ] = useState('Poppins') 
+
+   //Performing side effects (similar to lifecycle methods)
+   //timestamp: 36:22
+   // Example Side Effects for changing the title of the browser tab with Hooks
+   useEffect(() => {
+      document.title = name + ' ' + lastname
+      //not "this.state.name" is not used.  This is simpler. 
+   })
+   // useEffect is slightly different from componentDidMount as it will run after 
+   // the initial render AND every update.  this can be opted out of.
 
    const handleNameChange = (e) => {
       setName(e.target.value)
+   }
+   const handleLastnameChange = (e) => {
+      setLastname(e.target.value)
    }
 
    return (
@@ -20,6 +37,10 @@ export default function GreetingHookifiedFormInputs3() {
          <input
             value={name}
             onChange={handleNameChange}
+         /> 
+         <input
+            value={lastname}
+            onChange={handleLastnameChange}
          /> 
       </div>
    )
