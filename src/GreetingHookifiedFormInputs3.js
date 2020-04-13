e0f311ca9db7b86c5a669776eb2e1475183f7991
Jeremiah <39208179+jrodden1@users.noreply.github.com>
2020-04-13 16:25:12 -0600

    Refactored and abstracted FormInput state into custom hook useFormInput; simplifies form inputs greatly.

--- src/GreetingHookifiedFormInputs3.js
+++ src/GreetingHookifiedFormInputs3.js
@@ -11,30 +11,27 @@ export default function GreetingHookifiedFormInputs3() {
    // This will use React's local state -->> this is what "useState" does - use a local state for this component
    // useState is a "hook" -> a Hook is a fn provided by React that lets you "hook" into React features from your functional components
    // Whenever one of the states change or the setter method is called, then react will rerender the component just like it does with this.setState
-   const [ name, setName  ] = useState('Mary'/* Initial State is set here */)
-   const [ lastname, setLastname ] = useState('Poppins')
+   // const [ name, setName  ] = useState('Mary'/* Initial State is set here */)
+   const name = useFormInput('Mary')  //see useFormInput function declaration near bottom
+   // const [ lastname, setLastname ] = useState('Poppins')
+   const lastname = useFormInput('Poppins') //see useFormInput function declaration near bottom
+
    const width = useWindowWidth() // So actually, the order doesn't matter, it just needs to be in the top section.
    // this above subscribes us to current updates of the window's width
 
-   useDocumentTitle(name + ' ' + lastname) //function def below
-
-   const handleNameChange = (e) => {
-      setName(e.target.value)
-   }
-   const handleLastnameChange = (e) => {
-      setLastname(e.target.value)
-   }
+   // useDocumentTitle(name + ' ' + lastname) //function def below
+   useDocumentTitle(name.value + ' ' + lastname.value) // Updated to reflect new use of name as an object
 
    return (
       <div>
       Converted to a functional component, but also I can have statefulness:<br />
-         <input
-            value={name}
-            onChange={handleNameChange}
+         <input {...name} // since the name object has both value and onChange keys, you can simple spread the object on the input now.  Freakin' nifty!
+            // value={name.value}
+            // onChange={handleNameChange}
          /> 
-         <input
-            value={lastname}
-            onChange={handleLastnameChange}
+         <input {...lastname} // since the lastname object has both value and onChange keys, you can simple spread the object on the input now.  Freakin' nifty!
+            // value={lastname.value}
+            // onChange={handleLastnameChange}
          /> 
          <br />
          Width: {width}px (change window width to see me change)
@@ -77,3 +74,20 @@ function useDocumentTitle(title) {
    // useEffect is slightly different from componentDidMount as it will run after 
    // the initial render AND every update.  this can be opted out of.
 }
+
+// Timestamp: 49:20
+// abstracting the form input handlers into one generic one; 
+// this could be also used for validating other form info in a custom way
+// This helps to keep your component up top nice and clean / easy to read. 
+function useFormInput(initialValue) {
+   const [ value, setValue  ] = useState(initialValue)
+   const handleChange = (e) => {
+      setValue(e.target.value)
+   }
+   return {
+      value,
+      onChange: handleChange
+   }
+}
+// Also note that each time you call a hook, it will get its own local state
+// (so you can call a hook over and over and it will be ok)
